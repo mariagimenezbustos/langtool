9787e3fdb9ff425ae7d7ed7018e1b2b2fc6a855b
Maria Gimenez Bustos <134734638+mariagimenezbustos@users.noreply.github.com>
2025-02-21 17:52:52 +0100

    vid completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,46 +1,180 @@
 // Define graph here
+import fs from "fs";
+import { END, StateGraph } from "@langchain/langgraph";
+import { ChatOpenAI } from "@langchain/openai";
+import { extractCategory } from "tools/extract_category.js";
+import { DatasetSchema } from "types.js";
+import { TRIMMED_CORPUS_PATH } from "constants.js";
+import { selectApi } from "tools/select_api.js";
+import { extractParameters } from "tools/extract_parameters.js";
+import { requestParameters } from "tools/request_parameters.js";
+import { findMissingParams } from "util.js";
+import { createFetchRequest } from "tools/create_fetch_request.js";
 
 /**
  * TODO: implement
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type GraphState = any;
+export type GraphState = {
+ /**
+  * the LLM to use for the graph
+  */
+ llm: ChatOpenAI;
+ /**
+  * the query to extract an API for
+  */
+ query: string;
+ /**
+  * the relevant API categories for the query
+  */
+ categories: string[] | null;
+ /**
+  * the relevant APIs for the categories
+  */
+ apis: DatasetSchema[] | null;
+ /**
+  * the most relevant API for the query
+  */
+ bestApi: DatasetSchema | null;
+ /**
+  * the params for the API call
+  */
+ params: Record<string, string> | null;
+ /**
+  * the API response
+  */
+ // eslint-disable-next-line @typescript-eslint/no-explicit-any
+ response: Record<string, any> | null;
+};
 
-/**
- * TODO: implement
- */
-const graphChannels = {};
-
-/**
- * TODO: implement
- * @param {GraphState} state
- */
-const verifyParams = (): string => {
-  throw new Error("verifyParams not implemented");
+const graphChannels = {
+ llm: { value: null },
+ query: { value: null },
+ categories: { value: null },
+ apis: { value: null },
+ bestApi: { value: null },
+ params: { value: null },
+ response: { value: null },
 };
 
 /**
- * TODO: implement
+ * @param {GraphState} state
  */
+const verifyParams = (
+ state: GraphState
+): "human_loop_node" | "execute_request_node" => {
+ const { bestApi, params } = state;
+ if (!bestApi) {
+  throw new Error("No best API found");
+ }
+ if (!params) {
+  return "human_loop_node";
+ }
+ const requiredParamsKeys = bestApi?.required_parameters.map(
+  ({ name }) => name
+ );
+ const extractedParamsKeys = Object.keys(params);
+ const missingKeys = findMissingParams(requiredParamsKeys, extractedParamsKeys);
+ if (missingKeys.length > 0) {
+  return "human_loop_node";
+ }
+ return "execute_request_node";
+};
+
+function getApis(state: GraphState) {
+ const { categories } = state;
+ if (!categories || categories.length === 0) {
+  throw new Error("No categories passed to get_apis_node");
+ }
+ const allData: DatasetSchema[] = JSON.parse(
+  fs.readFileSync(TRIMMED_CORPUS_PATH, "utf8")
+ );
+
+ const apis = categories
+  ?.map((c) => allData.filter((d) => d.category_name === c))
+  .flat();
+
+ return {
+  apis,
+ };
+}
+
 function createGraph() {
-  throw new Error("createGraph not implemented");
+ const graph = new StateGraph<GraphState>({
+  channels: graphChannels,
+ });
+
+ graph.addNode("extract_category_node", extractCategory);
+ graph.addNode("get_apis_node", getApis);
+ graph.addNode("select_api_node", selectApi);
+ graph.addNode("extract_params_node", extractParameters);
+ graph.addNode("human_loop_node", requestParameters);
+ graph.addNode("execute_request_node", createFetchRequest);
+
+ graph.addEdge("extract_category_node", "get_apis_node");
+ graph.addEdge("get_apis_node", "select_api_node");
+ graph.addEdge("select_api_node", "extract_params_node");
+
+ graph.addConditionalEdges("extract_params_node", verifyParams);
+ graph.addConditionalEdges("human_loop_node", verifyParams);
+
+ graph.setEntryPoint("extract_category_node");
+ graph.setFinishPoint("execute_request_node");
+
+ const app = graph.compile();
+ return app;
 }
 
 const datasetQuery =
-  "I'm researching WhatsApp for Business accounts. Can you check if the number 9876543210 is a WhatsApp for Business account? Also, provide the business description, website, email, business hours, address, and category if it is.";
+ "I'm researching WhatsApp for Business accounts. Can you check if the number 9876543210 is a WhatsApp for Business account? Also, provide the business description, website, email, business hours, address, and category if it is.";
 
 const relevantIds = [
-  "8044d241-0f5b-403d-879a-48b080fd4bf6",
-  "a7c44eb0-c7f2-446a-b57e-45d0f629c50c",
-  "f657180c-3685-410d-8c71-a5f7632602f1",
+ "8044d241-0f5b-403d-879a-48b080fd4bf6",
+ "a7c44eb0-c7f2-446a-b57e-45d0f629c50c",
+ "f657180c-3685-410d-8c71-a5f7632602f1",
 ];
 
 /**
- * TODO: implement
  * @param {string} query
  */
 async function main(query: string) {
-  throw new Error("main not implemented" + query);
+ const app = createGraph();
+
+ const llm = new ChatOpenAI({
+  modelName: "gpt-4-turbo-preview",
+  temperature: 0,
+ });
+
+ const stream = await app.stream({
+  llm,
+  query,
+ });
+
+ let finalResult: GraphState | null = null;
+ for await (const event of stream) {
+  console.log("\n------\n");
+  if (Object.keys(event)[0] === END) {
+   console.log("---FINISHED---");
+   finalResult = event[END];
+  } else {
+   console.log("Stream event: ", Object.keys(event)[0]);
+   // to see the values of event in the console:
+   // console.log("Value(s): ", Object.values(event)[0]);
+  }
+ }
+
+ if (!finalResult) {
+  throw new Error("No final result");
+ }
+ if (!finalResult.bestApi) {
+  throw new Error("No best API found");
+ }
+ const resultHasProperIds = relevantIds.includes(finalResult.bestApi.id);
+ if (resultHasProperIds) {
+  console.log("✅✅✅ the result has the proper IDs ✅✅✅");
+ } else {
+  console.log("❌❌❌ the result does not have the proper IDs ❌❌❌");
+ }
 }
 
 main(datasetQuery);
--- backend/src/tools/create_fetch_request.ts
+++ backend/src/tools/create_fetch_request.ts
@@ -1,17 +1,80 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { GraphState } from "index.js";
 
 /**
- * TODO: implement & add args
- * @param {string} fetchRequestString
- */
-export function readUserVerification(): Promise<boolean> {
-  throw new Error("readUserVerification not implemented");
-}
-
-/**
- * TODO: implement & add args
  * @param {GraphState} state
  */
-export async function createFetchRequest(): Promise<Partial<GraphState>> {
-  throw new Error("createFetchRequest not implemented");
+export async function createFetchRequest(
+ state: GraphState
+): Promise<Partial<GraphState>> {
+ const { params, bestApi } = state;
+ if (!bestApi) {
+  throw new Error("No best API found");
+ }
+
+ let response: any = null;
+
+ try {
+  // if params are false, we're not going to add in any special query param
+  if (!params) {
+   // we can execute it with the request method and the URL
+   const fetchRes = await fetch(bestApi.api_url, {
+    method: bestApi.method,
+   });
+
+   // then get the response
+   response = fetchRes.ok ? await fetchRes.json() : await fetchRes.text();
+
+   // if we do pass in params
+  } else {
+   let fetchOptions: Record<string, any> = {
+    method: bestApi.method,
+   };
+
+   let parsedUrl = bestApi.api_url;
+
+   // first parse the URL to see if any of the params
+   // belong inside the URL and not as query parameters
+   // (if it exists, we add it in and then delete it from the params object)
+   const paramsKeys = Object.entries(params);
+   paramsKeys.forEach(([key, value]) => {
+    if (parsedUrl.includes(`{${key}}`)) {
+     parsedUrl = parsedUrl.replace(`{${key}}`, value);
+     delete params[key];
+    }
+   });
+
+   const url = new URL(parsedUrl);
+
+   // check if it's a GET or a HEAD request
+   // if so, there can be no body, so we don't add it in the fetch options
+   if (["GET", "HEAD"].includes(bestApi.method)) {
+    // format the URL with any extra query params which might exist
+    Object.entries(params).forEach(([key, value]) =>
+     url.searchParams.append(key, value)
+    );
+    // if it's not a GET or HEAD request, we need to add in the body
+   } else {
+    fetchOptions = {
+     ...fetchOptions,
+     body: JSON.stringify(params),
+    };
+   }
+
+   const fetchRes = await fetch(url, fetchOptions);
+   response = fetchRes.ok ? await fetchRes.json() : await fetchRes.text();
+  }
+
+  if (response) {
+   return {
+    response,
+   };
+  }
+ } catch (error) {
+  console.error("Error creating fetch request", error);
+ }
+
+ return {
+  response: null,
+ };
 }
